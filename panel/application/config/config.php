571fcb5348987e05daa5a0d8e4c442014ce2fd48
Ferzender Varli <fvarlii@gmail.com>
2020-04-10 21:03:35 +0300

    first commit

--- panel/application/config/config.php
+++ panel/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CMS/panel';
+$config['base_url'] = 'http://localhost/cms-application/panel';
 
 /*
 |--------------------------------------------------------------------------
